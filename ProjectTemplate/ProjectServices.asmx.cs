9d035ed2e1e3ae2172f6648dd084eca0d408964f
maximo canche <mcanche@asu.edu>
2022-07-24 15:26:15 -0600

    Finally got the log on working, when launching from the .html file you can use one of the userid and password combos in the database to get a message showing a successful log on or a failure. For some reason it works on the second try, I think it might be a sync thing but I dont know how to fix that.

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -34,6 +34,71 @@ namespace ProjectTemplate
 
 
 
+
+
+		//Maximo added this to try and get the log in functions working
+
+
+		//EXAMPLE OF A SIMPLE SELECT QUERY (PARAMETERS PASSED IN FROM CLIENT)
+		[WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
+		public bool LogOn(string UserID, string Password)
+		{
+			//we return this flag to tell them if they logged in or not
+			bool success = false;
+
+			//our connection string comes from our web.config file like we talked about earlier
+			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["440sum20224"].ConnectionString;
+			//here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+			//NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+			string sqlSelect = "SELECT UserID FROM Employee WHERE UserID=@idValue and Password=@passValue";
+
+			//set up our connection object to be ready to use our connection string
+			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+			//set up our command object to use our connection, and our query
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+			//tell our command to replace the @parameters with real values
+			//we decode them because they came to us via the web so they were encoded
+			//for transmission (funky characters escaped, mostly)
+			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(UserID));
+			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(Password));
+
+			//a data adapter acts like a bridge between our command object and 
+			//the data we are trying to get back and put in a table object
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			//here's the table we want to fill with the results from our query
+			
+
+			//Commented out by maximo for testing
+
+			
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+			//check to see if any rows were returned.  If they were, it means it's 
+			//a legit account
+			
+			if (sqlDt.Rows.Count > 0)
+			{
+				//if we found an account, store the id and admin status in the session
+				//so we can check those values later on other method calls to see if they 
+				//are 1) logged in at all, and 2) and admin or not
+				//Session["UserID"] = sqlDt.Rows[0]["id"];
+				//Session["admin"] = sqlDt.Rows[0]["admin"];
+				success = true;
+			}
+			
+			//return the result!
+			return success;
+			
+		}
+
+		// end what Maximo added
+
+
+
+
+
 		/////////////////////////////////////////////////////////////////////////
 		//don't forget to include this decoration above each method that you want
 		//to be exposed as a web service!
